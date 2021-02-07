c1fef72a56c24b51d296a6ae8601e57eed801ef4
Michal Dvorak <michal@mdvorak.org>
2021-02-07 18:26:51 +0100

    latest wps config

--- example/main.cpp
+++ example/main.cpp
@@ -27,9 +27,11 @@ void setup()
 	bool reconfigure = false;
 	ESP_ERROR_CHECK(double_reset_start(&reconfigure, DOUBLE_RESET_DEFAULT_TIMEOUT));
 
+	// Events
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+
 	// Initalize WiFi
 	ESP_ERROR_CHECK(esp_netif_init());
-	ESP_ERROR_CHECK(esp_event_loop_create_default());
 	esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
 	assert(sta_netif);
 	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
